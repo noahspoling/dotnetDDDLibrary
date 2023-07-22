a8c30cce43a6e65b7cb163e449d3b9db9e4c55c3
Noah Poling <noahspoling@gmail.com>
2023-07-22 18:37:16 -0400

    Started to work on DDD entities and infrastructure.
    Made some cool shell scripts for quickly cleaning and building projects.
    I don't have Visual Studio so these are nice to have.

---
+++ Bookstore.Domain/Entities/Book.cs
@@ -0,0 +1,44 @@
+//Book.cs
+
+using Bookstore.Domain.ValueObjects;
+
+namespace Bookstore.Domain.Entities
+{
+    public class Book
+    {
+        public int Id {get; set; }
+        public Title Title {get; set;}
+        public string Author {get; set;}
+        public Genre Genre {get; set;}
+        public string ISBN {get; set;}
+
+        public Book() {
+
+        }
+        public Book(Title title, Genre genre, string isbn) {
+            Title = title;
+            Genre = genre;
+            ISBN = isbn;
+        }
+        public Book(int id, Title title, Genre genre, string isbn) {
+            Id = id;
+            Title = title;
+            Genre = genre;
+            ISBN = isbn;
+        }
+        public Book(Title title, string author, Genre genre, string isbn) {
+            Title = title;
+            Author = author;
+            Genre = genre;
+            ISBN = isbn;
+
+        }
+        public Book(int id, Title title, string author, Genre genre, string isbn) {
+            Id = id;
+            Title = title;
+            Author = author;
+            Genre = genre;
+            ISBN = isbn;
+        }
+    }
+}
